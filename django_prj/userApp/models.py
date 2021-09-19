9d55bf16432178aad93a9755f3c205827ed10e8f
szaszgyozo <szaszgyozo@gmail.com>
2021-09-20 00:23:00 +0300

    creating models

--- django_prj/userApp/models.py
+++ django_prj/userApp/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.db.models import fields
+from django.db.models.enums import Choices
+from django.db.models.fields import CharField
 
 # Create your models here.
+
+class UserStatus(models.IntegerChoices):
+    ACTIVE = 1,
+    INACTIVE = 0
+
+class UserProfile(models.Model):
+    id = models.UUIDField(
+        primary_key=True, 
+        editable=False
+    )
+
+    phone_nr = models.CharField(
+        max_length=13, 
+        blank=True
+    )
+
+    age = models.IntegerField(
+        blank=True
+    )
+
+    status = models.IntegerField(
+        choices=UserStatus.choices,
+        default=UserStatus.ACTIVE
+    )
